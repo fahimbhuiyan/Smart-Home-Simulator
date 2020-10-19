170e303b484efdc78f43e6509b1cb1ccd1541a15
Mark Said <marksaid206@gmail.com>
2020-10-19 11:17:56 -0400

    JavaDoc
    Generated JavaDoc for Door, House and Light Model

--- src/sample/SmartHomeModel/HouseModel.java
+++ src/sample/SmartHomeModel/HouseModel.java
@@ -1,5 +1,8 @@
 package sample.SmartHomeModel;
 
+/**
+ * The House model.
+ */
 public class HouseModel {
 
     private double outsideTemp = 0;
@@ -7,31 +10,68 @@ public class HouseModel {
     private int id;
     private boolean simulationActive = false;
 
+    /**
+     * Instantiates a new House model.
+     *
+     * @param outsideTemp      the outside temp
+     * @param loggedUserName   the logged user name
+     * @param simulationActive the simulation active
+     */
     public HouseModel(int outsideTemp, String loggedUserName, boolean simulationActive) {
 
         this.loggedUserName = loggedUserName;
     }
 
+    /**
+     * Gets outside temp.
+     *
+     * @return the outside temp
+     */
     public double getOutsideTemp() {
         return outsideTemp;
     }
 
+    /**
+     * Sets outside temp.
+     *
+     * @param outsideTemp the outside temp
+     */
     public void setOutsideTemp(double outsideTemp) {
         this.outsideTemp = outsideTemp;
     }
 
+    /**
+     * Gets logged user name.
+     *
+     * @return the logged user name
+     */
     public String getLoggedUserName() {
         return loggedUserName;
     }
 
+    /**
+     * Sets logged user name.
+     *
+     * @param loggedUserName the logged user name
+     */
     public void setLoggedUserName(String loggedUserName) {
         this.loggedUserName = loggedUserName;
     }
 
+    /**
+     * Is simulation active boolean.
+     *
+     * @return the boolean
+     */
     public boolean isSimulationActive() {
         return simulationActive;
     }
 
+    /**
+     * Sets simulation active.
+     *
+     * @param simulationActive the simulation active
+     */
     public void setSimulationActive(boolean simulationActive) {
         this.simulationActive = simulationActive;
     }
--- src/sample/SmartHomeModel/LightModel.java
+++ src/sample/SmartHomeModel/LightModel.java
@@ -1,27 +1,55 @@
 package sample.SmartHomeModel;
 
+/**
+ * The Light model.
+ */
 public class LightModel {
 
 	private String id;
 	private boolean isOpen = false;
-	
+
+	/**
+	 * Instantiates a new Light model.
+	 *
+	 * @param id the id
+	 */
 	public LightModel(String id) {
 		super();
 		this.id = id;
 	}
 
+	/**
+	 * Gets id of the light.
+	 *
+	 * @return the id
+	 */
 	public String getId() {
 		return id;
 	}
 
+	/**
+	 * Sets id of the light.
+	 *
+	 * @param id the id
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
 
+	/**
+	 * Is light open.
+	 *
+	 * @return the boolean
+	 */
 	public boolean isOpen() {
 		return isOpen;
 	}
 
+	/**
+	 * Sets light to open.
+	 *
+	 * @param isOpen the is open
+	 */
 	public void setOpen(boolean isOpen) {
 		this.isOpen = isOpen;
 	}
