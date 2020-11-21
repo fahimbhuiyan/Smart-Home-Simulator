b342fb9b919a22cc65dbc82913f4a98eddcb0c2b
fahimbhuiyan <fahimrbhuiyan@gmail.com>
2020-11-21 03:44:08 -0500

    update on SHH & SHS
    for SHS, I added months and season along with items.
    for SHH, i added the items for each combo box
    I also extended the boxes so the UI is cleaner.

--- src/sample/SmartHomeController/MainViewController.java
+++ src/sample/SmartHomeController/MainViewController.java
@@ -531,7 +531,7 @@ public class MainViewController {
     ComboBox <String> periodComboBoxSHH;
 
     @FXML
-    ComboBox <String> temperatureComboBoxSHH;
+    Spinner <Integer> temperatureSpinnerSHH;
 
     @FXML
     Button saveSHH;
@@ -540,10 +540,19 @@ public class MainViewController {
     ComboBox <String> locationManComboBoxSHH;
 
     @FXML
-    ComboBox <String> temperatureManComboBoxSHH;
+    Spinner <Integer> temperatureManSpinnerSHH;
 
     @FXML
     Button saveManSHH;
+
+    @FXML
+    ComboBox <String> month;
+
+    @FXML
+    ComboBox <String> season;
+
+    @FXML
+    Button saveSeason;
 
     /**
      * A boolean indicating whether a user is logged into the simulation or not.
@@ -1247,6 +1256,13 @@ public class MainViewController {
             addModifyLocComboBoxSHS.getItems().addAll("Front yard", "Backyard");
             addModifyRoleComboBoxSHS.getItems().addAll("Parent", "Child", "Guest", "Stranger");
             timeSpeedComboBoxSHS.getItems().addAll("0.25", "0.5", "0.75", "1", "1.25", "1.5", "1.75", "2");
+            month.getItems().addAll("January", "February", "March", "April", "June", "July", "August", "September", "October", "November", "December");
+            season.getItems().addAll("Winter", "Summer");
+
+            zoneComboBoxSHH.getItems().addAll("Zone 1", "Zone 2", "Zone 3", "Zone 4");
+            locationComboBoxSHH.getItems().addAll("Garage", "Kitchen", "Bedroom", "Living Room", "Children's Room");
+            locationManComboBoxSHH.getItems().addAll("Garage", "Kitchen", "Bedroom", "Living Room", "Children's Room");
+            periodComboBoxSHH.getItems().addAll("00:00 - 08:00", "08:00 - 16:00", "16:00 - 24:00");
 
 
             addModifyLocComboBoxSHS.getSelectionModel().selectFirst();
@@ -1482,4 +1498,9 @@ public class MainViewController {
     public void saveManSHHSetting(ActionEvent event) {
 
     }
+
+    public void saveMonthSeason(ActionEvent event) {
+
+    }
+
 }
